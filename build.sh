5b6892a7fa4c0ca4e671564ece8ab3a7c296bcb4
krubot-sky <ben.eustace@sky.uk>
2021-02-25 13:26:19 +0000

    Adding some psp config to allow for admin pods

--- build.sh
+++ build.sh
@@ -2,52 +2,54 @@
 
 kubectl apply -f 00-namespace.yaml
 
-kubectl apply -f 01-nfs.yaml
+kubectl apply -f 01-permissions.yaml
 
-kubectl apply -f 02-docker.yaml
+kubectl apply -f 02-nfs.yaml
 
-kubectl apply -f 03-utils.yaml
+kubectl apply -f 03-docker.yaml
+
+kubectl apply -f 04-utils.yaml
 
 kubectl -n ledger wait --for=condition=complete --timeout=600s job/utils
 
-kubectl apply -f 04-orderer.yaml
+kubectl apply -f 05-orderer.yaml
 
 kubectl -n ledger wait --for=condition=available --timeout=600s deployment/orderer
 
-kubectl apply -f 05-ca.yaml
+kubectl apply -f 06-ca.yaml
 
 kubectl -n ledger wait --for=condition=available --timeout=600s deployment/ca
 
-kubectl apply -f 06-org1.yaml
+kubectl apply -f 07-org1.yaml
 
 kubectl -n ledger wait --for=condition=available --timeout=600s deployment/org1peer1
 
-kubectl apply -f 07-org2.yaml
+kubectl apply -f 08-org2.yaml
 
 kubectl -n ledger wait --for=condition=available --timeout=600s deployment/org2peer1
 
-kubectl apply -f 08-org3.yaml
+kubectl apply -f 09-org3.yaml
 
 kubectl -n ledger wait --for=condition=available --timeout=600s deployment/org3peer1
 
-kubectl apply -f 09-org4.yaml
+kubectl apply -f 10-org4.yaml
 
 kubectl -n ledger wait --for=condition=available --timeout=600s deployment/org4peer1
 
-kubectl apply -f 10-create-channel.yaml
+kubectl apply -f 11-create-channel.yaml
 
 kubectl -n ledger wait --for=condition=complete --timeout=600s job/create-channel
 
-kubectl apply -f 11-join-channel.yaml
+kubectl apply -f 12-join-channel.yaml
 
 kubectl -n ledger wait --for=condition=complete --timeout=600s job/join-channel
 
-kubectl apply -f 12-install-chaincode.yaml
+kubectl apply -f 13-install-chaincode.yaml
 
 kubectl -n ledger wait --for=condition=complete --timeout=600s job/chaincode-install
 
-kubectl apply -f 13-instantiate-chaincode.yaml
+kubectl apply -f 14-instantiate-chaincode.yaml
 
 kubectl -n ledger wait --for=condition=complete --timeout=600s job/chaincode-instantiate
 
-kubectl apply -f 14-application.yaml
+kubectl apply -f 15-application.yaml
--- delete.sh
+++ delete.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-kubectl delete -f 14-application.yaml
+kubectl delete -f 15-application.yaml
 
-kubectl delete -f 13-instantiate-chaincode.yaml
+kubectl delete -f 14-instantiate-chaincode.yaml
 
-kubectl delete -f 12-install-chaincode.yaml
+kubectl delete -f 13-install-chaincode.yaml
 
-kubectl delete -f 11-join-channel.yaml
+kubectl delete -f 12-join-channel.yaml
 
-kubectl delete -f 10-create-channel.yaml
+kubectl delete -f 11-create-channel.yaml
 
-kubectl delete -f 09-org4.yaml
+kubectl delete -f 10-org4.yaml
 
-kubectl delete -f 08-org3.yaml
+kubectl delete -f 09-org3.yaml
 
-kubectl delete -f 07-org2.yaml
+kubectl delete -f 08-org2.yaml
 
-kubectl delete -f 06-org1.yaml
+kubectl delete -f 07-org1.yaml
 
-kubectl delete -f 05-ca.yaml
+kubectl delete -f 06-ca.yaml
 
-kubectl delete -f 04-orderer.yaml
+kubectl delete -f 05-orderer.yaml
 
-kubectl delete -f 03-utils.yaml
+kubectl delete -f 04-utils.yaml
 
-kubectl delete -f 02-docker.yaml
+kubectl delete -f 03-docker.yaml
 
-kubectl delete -f 01-nfs.yaml
+kubectl delete -f 02-nfs.yaml
+
+kubectl delete -f 01-permissions.yaml
+
+kubectl -n ledger delete pvc docker-docker-dind-0 nfs-nfs-server-0
